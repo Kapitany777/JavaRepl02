25b957f62c246daa1df4f41ae73759a798c303b9
Török Viktor <kapitany777@gmail.com>
2026-04-28 13:54:16 +0200

    A parancsokat map-ben tároljuk

--- src/eu/braincluster/javarepl02/Main.java
+++ src/eu/braincluster/javarepl02/Main.java
@@ -1,31 +1,49 @@
 package eu.braincluster.javarepl02;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
 
+	private static Map<String, Runnable> commands = new HashMap<>();
+	private static boolean running = true;
+	
 	public static void main(String[] args) {
 		System.out.println("Java Stack program");
+		
+		initCommands();
 
 		var scanner = new Scanner(System.in);
 
-		while (true) {
+		while (running) {
 			System.out.println("> ");
 			String command = scanner.nextLine();
 
-			if ("exit".equalsIgnoreCase(command)) {
-				System.out.println("Kilépés");
-				break;
-			}
-			else if ("help".equalsIgnoreCase(command)) {
-				printHelp();
-			}
-			else {
-				System.out.println("Hibás parancs! Írd be: help");
-			}
+			handleCommand(command);
 		}
 
 		scanner.close();
+	}
+	
+	private static void initCommands() {
+		commands.put("help", () -> printHelp());
+		
+		commands.put("exit", () -> {
+			System.out.println("Kilépés...");
+			running = false;
+		});
+	}
+	
+	private static void handleCommand(String input) {
+		var command = commands.get(input.toLowerCase());
+		
+		if (command != null) {
+			command.run();
+		}
+		else {
+			System.out.println("Hibás parancs!");
+		}
 	}
 
 	private static void printHelp() {
